3b736fad303a5ba5d52d4c7df4d6fa781423f027
ryanv249 <ryanv249@bu.edu>
2022-12-06 19:17:46 -0500

    added padding, support for size overrides to Button class

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,16 @@
-import { Display } from "phaser";
-
 export default class Button{
     /**
      * Creates a Bitmap Text GO with the passed font and label with a Rectangle Shape GO as a background.
-     * The background rectangle handles interaction, and it is scaled to fit the text.
-     * When arranging, move rectangle and then use Align() function to place text correctly. 
+     * The background rectangle handles interaction, and it is scaled to fit the text + padding.
+     * Can force rectangle size using forceW and forceH params. Use Align() to center text.
      * 
      * @param {Phaser.Scene} scene 
      * @param {number} xPos 
      * @param {number} yPos 
      * position of background
+     * @param {number} forceW
+     * @param {number} forceH
+     * background size overrides, for scaled size, pass -1.
      * @param {number} color 
      * color of background
      * @param {number} textSize
@@ -20,18 +21,30 @@ export default class Button{
      * @param {*} offHover 
      * interactive callbacks
      */
-    constructor(scene, xPos, yPos, color, textSize, font, label, onClick, onHover, offHover){
+    constructor(scene, xPos, yPos, forceW, forceH, color, textSize, font, label, onClick, onHover, offHover){
         // add objects to scene
         // rectangle starting size of 0,0 very important.
         this.background = scene.add.rectangle(xPos, yPos, 0, 0, color);
         this.text = scene.add.bitmapText(0, 0, font, label, textSize);
 
-        // set base background size to match text
-        this.background.width = this.text.width;
-        this.background.height = this.text.height;
+        // set base background size
+        if(forceW !== -1 || forceH !== -1){
+            // some dimension (or both) overriden
+            this.background.width = forceW === -1 ? this.text.width : forceW;
+            this.background.height = forceH === -1 ? this.text.height : forceH;
+        }
+        else{
+            // background fully scaled to text
+            this.background.width = this.text.width*1.1;
+            this.background.height = this.text.height*1.1;
+        }
+
+        // set default offsets for Align() function
+        this.offsetX = this.text.width*0.005;
+        this.offsetY = this.text.height*0.15;
 
         // set rectangle to be interactable
-        // cannot do it when adding rectangle since size is not set yet, bad stuff happens
+        // cannot do it when adding rectangle since size is not set yet; bad stuff happens
         this.background
             .setOrigin(0)
             .setInteractive({useHandCursor: true})
@@ -40,10 +53,21 @@ export default class Button{
             .on('pointerout', () => offHover());
     }
 
-    Align(){
-        // place text in middle of background.
-        // offset necessary due to built-in margins in bitmaptext object
-        Phaser.Display.Align.In.Center(this.text, this.background, this.text.width*0.005, this.text.height*0.1);
+    
+    /**
+     * Center text within its background. Necessary to call whenever rectangle moved!
+     * @param {number} offX 
+     * x offset. default value:     this.text.width*0.005
+     * @param {number} offY 
+     * y offset. default value:     this.text.height*0.15
+     */
+    Align(offX, offY){
+        // offset recommended due to built-in margins in bitmaptext object. 
+        // default offset usually good enough for 1-line strings.
+        Phaser.Display.Align.In.Center(this.text, this.background,
+            offX === undefined ? this.offsetX : offX,
+            offY === undefined ? this.offsetY : offY
+            );
     }
 
 }
--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -16,15 +16,23 @@ export default class MainMenu extends Phaser.Scene {
       new TitleText(this);
 
       // create menu buttons
-      const b = new Button(this, 0, 0, 0xff5500, 90, 'main_font_b', 'button1', 
-      () => console.log('clicked'),
-      () => console.log('over'),
-      () => console.log('off'));
+      const b1 = new Button(this, 0, 0, -1, -1, 0xff5500, 40, 'main_font_b', 'afiafsasfasfs\niausdhiuahias', 
+      () => console.log('clicked1'),
+      () => console.log('over1'),
+      () => console.log('off1'));
+
+      const b2 = new Button(this, 0, 0, -1, -1, 0xff5500, 90, 'main_font_b', 'button2', 
+      () => console.log('clicked2'),
+      () => console.log('over2'),
+      () => console.log('off2'));
+
 
       // arrange buttons on screen
       let menu = this.add.rectangle(400, 300, 200, this.height, 0x00, 0);
-      Phaser.Display.Align.In.Center(b.background, menu);
-      b.Align();
+      Phaser.Display.Align.In.Center(b1.background, menu);
+      b1.Align(0,b2.offsetY*0.5);
+      Phaser.Display.Align.To.BottomCenter(b2.background, b1.background, 0, 40);
+      b2.Align();
 
     }
 
